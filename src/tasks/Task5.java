4912015b4b308793e96198cfd1105eb5c39d37da
Damir <vip.ushkempir@mail.ru>
2025-03-27 14:56:00 +0500

    Added Task5, validation for each task Configured menu

--- src/tasks/Task5.java
+++ src/tasks/Task5.java
@@ -2,10 +2,10 @@ package tasks;
 
 import java.util.Scanner;
 
-public class Task1 {
+public class Task5 {
     public static void problem() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("You are given a number “n” and an array of “n” elements, write the function that returns minimum of them.");
+        System.out.println("You are given a number “n”, write the function for finding n-th elements in Fibonacci sequence using recursion. (Fn = Fn-1+ Fn-2). F0 = 0, F1 = 1.");
 
         int n;
         while (true) {
@@ -23,29 +23,17 @@ public class Task1 {
             }
         }
 
-        int[] arr = new int[n];
-
-        System.out.print("Enter " + n + " elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
         double startTime = System.nanoTime();
-        int result = findMin(arr);
+        int result = fib(n);
         double endTime = System.nanoTime();
         double duration = (endTime - startTime) / 1000000;
 
-        System.out.println("The minimum is: " + result);
+        System.out.println("Fibonacci number at position " + n + " is " + result);
         System.out.println("Time taken: " + duration + " milliseconds");
     }
 
-    private static int findMin(int[] arr) {
-        int min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        return min;
+    private static int fib(int n) {
+        if (n == 0 || n == 1) { return n; }
+        return fib(n - 1) + fib(n - 2);
     }
 }
